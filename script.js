765fc41db6c55c6fe9ec2b88d078aa02aa87595d
mercylifejpg <murayacae0026@kist.ac.ke>
2025-04-15 17:08:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,9 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
         errorMsg.textContent = "Invalid username or password.";
     }
 });
+
+document.getElementById('logout').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Redirect back to the login page
+    window.location.href = "index.html"; // Change to your login page
+});
