a786afeba059143f564ba25809234bfd74611376
mercylifejpg <murayacae0026@kist.ac.ke>
2025-04-15 17:53:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -102,4 +102,7 @@ function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
     
-    if (n > slides.length
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    
+    for (
